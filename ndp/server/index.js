2afc75979a19775831c90f5f8d05d53cfc436bfb
Alex Johnson <david.johnsoniv@gmail.com>
2017-09-16 09:24:04 -0600

    modified index

--- ndp/server/index.js
+++ ndp/server/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var server = require('./controllers/serverController')
 
 var app = express();
 
 app.use(bodyParser.json());
+
+
+
+
 
 var port = 3001;
 app.listen(port, () => {console.log("Server is g2g on port " + port + ".")})
